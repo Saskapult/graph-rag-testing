a1060ec1f9e3b8bc9d928e224e9757469317deb9
Saskapult <thekieransmith@gmail.com>
2025-04-06 23:52:39 -0700

    fix test

--- test_timeout_requeue.sh
+++ test_timeout_requeue.sh
@@ -20,9 +20,11 @@ trap 'sig_handler_USR1' SIGUSR1
 
 if [ -f "timeout_requeue_marker" ]; then
 	echo "Marker exists"
+	touch "timeout_requeue_marker_2"
 else
 	echo "Marker not exists, sleep"
-	sleep 120
+	sleep 120 &
+	wait
 	echo "Unreachable 1"
 fi
 
