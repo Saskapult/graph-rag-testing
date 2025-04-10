b7924d62ebf29f4ed6eaf1b54ff3ec9399d2a162
Saskapult <thekieransmith@gmail.com>
2025-04-10 11:47:37 -0700

    improve MINE analysis tool

--- mine_results.py
+++ mine_results.py
@@ -3,15 +3,18 @@ import json
 import storage
 
 output_dir = "kg-gen-repo/MINE/KGs"
+sus_threshold = 0.1
 
 
 def read_results_json(results_dir):
-	results = []
+	filenames = []
+	contents = []
 	for f in os.listdir(results_dir):
 		if f.endswith("_results.json"):
-			result = read_json(results_dir + "/" + f)
-			results.append(result)
-	return results
+			filenames.append(f)
+			content = read_json(results_dir + "/" + f)
+			contents.append(content)
+	return filenames, results
 
 
 def result_sum(results):
@@ -24,16 +27,27 @@ def result_sum(results):
 
 
 def main():
-	files = read_results_json(output_dir)
+	names, files = read_results_json(output_dir)
+
+	suspicious = []
 	scores_accuracy_sum = 0.0
-	for file in files:
+	for name, file in zip(names, files):
 		count, score_sum = result_sum(file)
-		scores_accuracy_sum += score_sum / count
-		print(f"Score {score_sum} / {count} ({(score_sum/count*100):.2f}%)")
-	
+		accuracy = score_sum / count
+		scores_accuracy_sum += accuracy
+		print(f"{name} score {score_sum} / {count} ({(score_sum/count*100):.2f}%)")
+		if accuracy <= sus_threshold:
+			print("\tThat's suspicious")
+			suspicious.append((name, accuracy))
+
 	# Floating point truncation probably is not an issue here
 	scores_accuracy_sum /= len(files)
 	print(f"Average accuracy is {scores_accuracy_sum*100:.2f}%")
+
+	if len(suspicious) > 0:
+		print(f"Found {len(suspicious)} results with accuracy less than {sus_threshold*100:.2f}%:")
+		for name, accuracy in suspicious:
+			print(f"'{name}' - {accuracy*100:.2f}%")
 
 
 if __name__ == "__main__":
