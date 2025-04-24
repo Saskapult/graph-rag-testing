981abb98303d2ece944d899c2884fe692d499c75
Saskapult <thekieransmith@gmail.com>
2025-04-24 16:05:59 -0700

    amend

--- process_input.sh
+++ process_input.sh
@@ -48,7 +48,7 @@ apptainer instance start \
 
 echo "Running script"
 # Execute in background so that the signal interrupt works
-srun apptainer exec instance://ollama-phi4 uv run process.py -ai -o "$OUTDIR" "$INPUT" &
+srun apptainer exec instance://ollama-phi4 uv run process.py -a -o "$OUTDIR" "$INPUT" &
 wait 
 
 echo "Stopping apptainer"
