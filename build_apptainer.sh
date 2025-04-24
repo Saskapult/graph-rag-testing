44ffdb73d350aec1dec0c4aca49d8d880f6613ea
Saskapult <thekieransmith@gmail.com>
2025-04-24 15:57:35 -0700

    remove index

--- build_apptainer.sh
+++ build_apptainer.sh
@@ -5,4 +5,4 @@
 # Doesn't need to be a job, executes fine otherwise
 
 module load apptainer
-time APPTAINER_NO_MOUNT=tmp apptainer build ollama-phi4.sif ollama-phi4.def
+time APPTAINER_NO_MOUNT=tmp apptainer build -F ollama-phi4.sif ollama-phi4.def
